74824613b43fe7a7ee2c718c5a135ef552446a39
BT_1N3_21@bktech.local <BT_1N3_21@bktech.local>
2018-12-18 10:13:17 -0500

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,13 @@
+public class Runner {
+    public static void main(String[] args)
+    {
+        TimeInterval one=new TimeInterval(8,10);
+        TimeInterval two=new TimeInterval(9,12);
+
+        Appointment OneA=new Appointment(one);
+        Appointment TwoB=new Appointment(two);
+
+        System.out.println(OneA.conflictsWith(TwoB));
+        DailySchedule today=new DailySchedule();
+    }
+}
